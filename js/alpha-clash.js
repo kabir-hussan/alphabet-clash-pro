3a465c4eec26cfda2e122ebb1fab09e2174ed81c
kabir-hussan <kabirhussan891@gmail.com>
2024-02-18 00:59:59 +0600

    hide home section and show playground section done with utility js

--- js/alpha-clash.js
+++ js/alpha-clash.js
@@ -1,3 +1,19 @@
+// direct way
+// function playNow(){
+//     //get home screen and hide it
+//     const homeSection= document.getElementById('home-screen'); 
+//     // hide home screen by click playNow
+//     homeSection.classList.add('hidden');
+
+//     // get playground section and show it
+//     const playGroundSection= document.getElementById('play-ground');
+//     playGroundSection.classList.remove('hidden');
+
+
+// }
+
+// Indirect way just calling function
 function playNow(){
-    console.log("start to play");
+    hideElementById('home-screen');
+    showElementById('play-ground');
 }
