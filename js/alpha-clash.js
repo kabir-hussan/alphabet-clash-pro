3638d5d36c66023d596039db943d53ecd16cf8d3
kabir-hussan <kabirhussan891@gmail.com>
2024-02-18 02:05:03 +0600

    keyboard alphabet background and show random alphabet on the screen done

--- js/alpha-clash.js
+++ js/alpha-clash.js
@@ -12,8 +12,24 @@
 
 // }
 
+// function for continue the game
+function continueGame(){
+
+    // find random alphabet
+    const randomAlphabet= getRandomAlphabets();
+    // show random alphabet on the screen
+    const currentScreenAlphabet= document.getElementById('screen-alphabet');
+    currentScreenAlphabet.innerText= randomAlphabet;
+
+    // call set backgroundColor of alphabet function
+    setBackgroundColorById(randomAlphabet);
+
+}
+
 // Indirect way just calling function
 function playNow(){
     hideElementById('home-screen');
     showElementById('play-ground');
+    continueGame();
+
 }
--- js/utility.js
+++ js/utility.js
@@ -7,3 +7,24 @@ function showElementById(elementId){
     const element= document.getElementById(elementId);
     element.classList.remove('hidden');
 }
+// set background color of keyboard alphabet
+
+function setBackgroundColorById(elementId){
+    const element= document.getElementById(elementId);
+    element.classList.add('bg-[#FFA500]');
+}
+
+// generate random alphabets
+
+function getRandomAlphabets(){
+    const alphabetsString= 'abcdefghijklmnopqrstuvwxyz';
+    const alphabets= alphabetsString.split('');
+    
+    // random index between 0 to 25
+    const randomIndex= Math.random()*25;
+    const index= Math.round(randomIndex);
+    
+    // find random alphabet
+    const alphabet= alphabets[index];
+    return alphabet;
+}
